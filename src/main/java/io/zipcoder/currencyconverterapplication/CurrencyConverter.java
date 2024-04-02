2b8e115740e67b22a1642ee08c020b1a325bc7bc
Abukarabukare <Abukarshegow@gmail.comm>
2024-04-02 12:43:51 -0400

    not yet

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -1,5 +1,7 @@
 package io.zipcoder.currencyconverterapplication;
 
+
+//1
 public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
         return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
